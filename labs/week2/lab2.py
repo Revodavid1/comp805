916bb0bf7ee90ce53fd2f27462b767a3840a25bb
Oreva <orevadavidomu@gmail.com>
2018-01-30 19:32:42 -0500

    implement squared_nums

---
+++ labs/week2/lab2.py
@@ -0,0 +1,26 @@
+"""
+lab2.py
+Oreva David Omu
+1/30/2018
+"""
+def squared(num_list):
+	"""
+	Squares numbers in num_list
+	num_list:list of numbers
+	Returns:list of these numbers squared
+	"""
+	squarenumlist = [ ] #define result list
+	for nums in num_list: #iterate through num_list
+		squarenum = pow(nums,2) #square the numbers
+		squarenumlist.append(squarenum) #add the number to the result list
+	return squarenumlist
+
+def check_title(title_list):
+	"""
+	Removes strings in title_list that have numbers and aren't title case
+	title_list: list of strings
+	Returns: list of strings that are titles
+	"""
+
+	newstrlist = [ ]
+	
