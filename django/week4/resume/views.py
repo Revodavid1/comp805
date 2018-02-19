6ca099b2b4534ca79c7a3b7d7ce42b20a57e3967
Oreva <orevadavidomu@gmail.com>
2018-02-19 02:10:10 -0500

    edited views to get first resume

--- django/week4/resume/views.py
+++ django/week4/resume/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render
-from .models import Education, Experience
+from .models import Education, Experience, Resume
 # Create your views here.
 
 def home(request):
     """
     renders the resume home template
     """ 
-    name = "Omu Oreva"
     address = "400 Commercial Street, Manchester, NH, 03101"
     phone = "(603) 289-0134"
     email = "odo1001@wildcats.unh.edu"
     skills = ["Django","Python"]
     context = {}
-    educations = Education.objects.order_by('degree')
+    first_resume = Resume.objects.first()
     experiences = Experience.objects.order_by('title')
-    context = {'my_education':educations,'my_experiences':experiences,
-    'my_name':name,'my_addr':address,'my_phone':phone,"my_email":email,
-    "my_skills":skills}
+    context = {'my_addr':address,'my_phone':phone,"my_email":email,
+    'my_skills':skills,'my_res':first_resume}
     return render(request,'resume/home.html',context)
